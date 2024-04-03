118b98b2fec34b2bd381cd459d994c6ff1616914
Gustavo Aguiar <gustavoaguiar14325@gmail.com>
2024-04-03 13:03:35 -0300

    Classe FiladeEspera criada

---
+++ codigo/FiladeEspera.cs
@@ -0,0 +1,44 @@
+internal class FiladeEspera
+{
+  private int capacidadeMesa;
+  private Queue<ReqMesa> requisicoes = new Queue<ReqMesa>();
+
+    /// <summary>
+    ///  Adiciona requisição à fila de requisições
+    /// </summary>
+    /// <param name="req">ReqMesa para enfileirar</param>
+
+  public void AddRequisicao(ReqMesa req)
+  {
+    requisicoes.Enqueue(req);
+  }
+
+  /// <summary>
+  /// Remove requisição da fila e manda uma mensagem de erro se a fila estiver vazia
+  /// </summary>
+  /// <returns>Requisição removida</returns>
+  /// <exception cref="Exception">Se a fila estiver vazia, é enviado "Fila vazia"</exception>
+
+  public ReqMesa RemoverRequisicao()
+  {
+    if (requisicoes.Count == 0)
+    {
+      throw new Exception("Fila vazia");
+    }
+
+    ReqMesa reqRemovida = requisicoes.Dequeue();
+    return reqRemovida;
+  }
+
+  /// <summary>
+  /// Percorre a fila para ver em qual a posição dos clientes
+  /// </summary>
+
+  public void VerFila()
+  {
+    foreach (ReqMesa r in requisicoes)
+    {
+      Console.WriteLine(r.nomeCliente);
+    }
+  }
+}
