2fb4ce0c94d56e7ac48f62d81e9032b8101614f3
Danilo Loose <danilo.loose@digibee.com>
2024-04-11 17:39:06 -0300

    cria lista de clientes, requisições e logica de colocar cliente na fila

--- trabalho-poo-01/codigo/Restaurante.cs
+++ trabalho-poo-01/codigo/Restaurante.cs
@@ -1,51 +1,82 @@
 class Restaurante{
     private List<Mesa> mesas;
-    private List<FilaEspera> filaDeEspera;
+    private List<FilaEspera> filasDeEspera;
+    private List<ReqMesa> reqMesa;
+    private List<Cliente> clientes;
 
     public Restaurante(){
         this.mesas = new List<Mesa>();
-        this.filaDeEspera = new List<FilaEspera>();
+        this.filasDeEspera = new List<FilaEspera>();
 
         CriarMesas(4, 4); // 4 mesas de capacidade 4
         CriarMesas(4, 6); // 4 mesas de capacidade 6
         CriarMesas(2, 8); // 2 mesas de capacidade 8
 
-        filaDeEspera.Add(new FilaEspera(4));
-        filaDeEspera.Add(new FilaEspera(6));
-        filaDeEspera.Add(new FilaEspera(8));
+        filasDeEspera.Add(new FilaEspera(4));
+        filasDeEspera.Add(new FilaEspera(6));
+        filasDeEspera.Add(new FilaEspera(8));
+    }
+
+    public void CadastrarCliente(Cliente cliente)
+    {
+        clientes.Add(cliente);
+    }
+
+    public void AtenderCliente(Cliente cliente)
+    {
+        ReqMesa req = new ReqMesa(cliente.QtdPessoas, cliente.Nome);
+        reqMesa.Add(req);
+        AlocarMesa(req);
     }
     
-    public void AlocarMesa(int qtdPessoa, string nome){
+    public void AlocarMesa(ReqMesa req){
         bool mesaAlocada = false;
         foreach(Mesa mesa in mesas){
-            if(mesa.capacidade <= qtdPessoa && !mesa.isOcupada){
-                mesa.ocuparMesa();
+            if(req.qtdPessoa <= mesa.capacidadeMaxima && !mesa.estaOcupada){
+                mesa.OcuparMesa();
                 mesaAlocada = true;
+                req.mesa = mesa;
                 break;
             }
         }
 
         if(!mesaAlocada){
-            Mesa mesa = mesas.Find(mesa => mesa.capacidade <= qtdPessoa);
-            ReqMesa req = new ReqMesa(qtdPessoa, nome, mesa);
+            AlocarNaFilaEspera(req); 
         }
 
     }
-    public void DesalocarDaFilaEspera (int idReq){
-        foreach(FilaEspera fila in filaDeEspera){
-            foreach(ReqMesa req in fila.requisicoes){
-                if(req.idReq == idReq){
-                    req.FecharRequisicao();
-                    fila.RemoveRequisicao(req);
-                    return;
-                }
+
+    private void AlocarNaFilaEspera(ReqMesa reqMesa)
+    {
+        FilaEspera filaEscolhida = new FilaEspera(0);
+        bool foiEscolhida = false;
+        foreach(FilaEspera fila in filasDeEspera)
+        {
+            if(reqMesa.qtdPessoas <= 4 && fila.capacidadeMesa == 4)
+            {
+                filaEscolhida = fila;
+                foiEscolhida = true;
+                break;
+            }else if(reqMesa.qtdPessoas <= 6 && fila.capacidadeMesa == 6)
+            {
+                filaEscolhida = fila;
+                foiEscolhida = true;
+                break;
+            } else if(reqMesa.qtdPessoas <= 8 && fila.capacidadeMesa == 8)
+            {
+                filaEscolhida = fila;
+                foiEscolhida = true;
+                break;
             }
         }
+
+        if(foiEscolhida)
+        {
+            filaEscolhida.AddRequisicao(reqMesa);
+        }
+
     }
-    private void AlocarNaFilaEspera (ReqMesa reqMesa){
-        FilaEspera fila = filaDeEspera.Find(fila => fila.capacidadeMesa <= qtdPessoa);
-        fila.AddReq(reqMesa);
-    }
+
 
     private void CriarMesas(int qtdMesas, int qtdPessoas){
         for ( int i = 0; i < qtdMesas; i++){
