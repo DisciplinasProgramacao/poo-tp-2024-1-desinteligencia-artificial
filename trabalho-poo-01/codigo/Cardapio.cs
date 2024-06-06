8af17411cb2703240ec89ba025d2debf0148ea57
Daniel-Edu-61 <1474616@sga.pucminas.br>
2024-06-06 18:12:57 -0300

    Alterações nas classes Cardápio e Produto

--- trabalho-poo-01/codigo/Cardapio.cs
+++ trabalho-poo-01/codigo/Cardapio.cs
@@ -16,25 +16,29 @@ class Cardapio
             new Produto(2, "Falafel Assado", 20.00, "Bolinhos de grão-de-bico assados, temperados com ervas e especiarias."),
             new Produto(3, "Salada Primavera com Macarrão Konjac", 25.00, "Salada fresca com macarrão konjac e vegetais da primavera."),
             new Produto(4, "Escondidinho de Inhame", 18.00, "Purê de inhame com recheio de vegetais e cogumelos."),
-            new Produto(5, "Strogonoff de Cogumelos", 35.00, "Cogumelos em molho cremoso de leite de coco ou creme de castanhas."),
-            new Produto(6, "Caçarola de legumes", 22.00, "Vegetais variados assados em molho de tomate e ervas."),
+            new Produto(5, "Strogonoff de Cogumelos", 30.00, "Cogumelos em molho cremoso de leite de coco ou creme de castanhas."),
+            new Produto(6, "Caçarola de legumes", 24.00, "Vegetais variados assados em molho de tomate e ervas."),
             new Produto(7, "Água", 3.00, "Bebida pura e fresca."),
             new Produto(8, "Copo de suco", 7.00, "Suco natural de frutas frescas."),
             new Produto(9, "Refrigerante orgânico", 7.00, "Refrigerante feito com ingredientes orgânicos."),
-            new Produto(10, "Cerveja vegana", 9.00, "Cerveja sem produtos de origem animal."),
-            new Produto(11, "Taça de vinho vegano", 18.00, "Vinho produzido sem clarificantes de origem animal.")
+            new Produto(10, "Cerveja vegana", 12.00, "Cerveja sem produtos de origem animal."),
+            new Produto(11, "Taça de vinho vegano", 29.00, "Vinho produzido sem clarificantes de origem animal.")
         };
     }
 
     /// <summary>
     /// Mostra todas as opções do cardápio.
     /// </summary>
-    public void MostrarOpcoes()
+    public string MostrarCardapio()
     {
+        string cardapio = "Cardápio:\n";
+
         foreach (var produto in produtos)
         {
-            Console.WriteLine($"{produto.Id}. {produto.Nome} – R$ {produto.Valor}");
+           cardapio += $"{produto.GetId()}. {produto.GetNome()} – R$ {produto.GetValor()}\n";
         }
+
+         return cardapio;
     }
 
     /// <summary>
@@ -44,6 +48,6 @@ class Cardapio
     /// <returns>O produto correspondente ao ID, ou null se não for encontrado.</returns>
     public Produto ObterProduto(int id)
     {
-        return produtos.Find(p => p.Id == id);
+        return produtos.Find(p => p.GetId() == id);
     }
 }
