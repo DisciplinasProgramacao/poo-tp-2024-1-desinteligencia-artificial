07792b289c7c896e0045647fdf9ae2f4d380533c
miguelbizzi <miguel.bizzi@aglets.com.br>
2024-06-13 00:33:14 -0300

    feat: change cardapio

--- trabalho-poo-01/codigo/Cafe.cs
+++ trabalho-poo-01/codigo/Cafe.cs
@@ -38,4 +38,24 @@ class Cafe : Loja
 
         return false;
     }
+
+    protected override Cardapio CriarCardapio()
+    {
+        List<Produto> produtos = new List<Produto>
+        {
+            new Produto(1, "Não de queijo", 5, "Delicioso pão com recheio cremoso de queijo derretido"),
+            new Produto(2, "Bolinha de cogumelo", 7, "Bolinhas crocantes recheadas com cogumelos frescos e temperos especiais"),
+            new Produto(3, "Rissole de palmito", 7, "Massa crocante recheada com palmito macio e temperos selecionados"),
+            new Produto(4, "Coxinha de carne de jaca", 8, "Coxinha vegana com recheio suculento de carne de jaca desfiada"),
+            new Produto(5, "Fatia de queijo de caju", 9, "Fatia generosa de queijo vegano feito com castanha de caju"),
+            new Produto(6, "Biscoito amanteigado", 3, "Biscoito caseiro amanteigado, perfeito para acompanhar um café"),
+            new Produto(7, "Cheesecake de frutas vermelhas", 15, "Deliciosa cheesecake com uma combinação irresistível de frutas vermelhas"),
+            new Produto(8, "Água", 3, "Água mineral pura e refrescante"),
+            new Produto(9, "Copo de suco", 7, "Copo de suco natural feito com frutas frescas e sem adição de açúcar"),
+            new Produto(10, "Café espresso orgânico", 6, "Café espresso de alta qualidade, cultivado de forma orgânica e com aroma intenso")
+        };
+
+        cardapio = new Cardapio(produtos);
+        return cardapio;
+    }
 }
