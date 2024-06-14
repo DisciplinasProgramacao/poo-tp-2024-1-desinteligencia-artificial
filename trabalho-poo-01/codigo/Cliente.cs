b2eadbc0e469e3556069fbecca1f06086cee0c8f
smkleite <contatosmk1@gmail.com>
2024-06-13 22:15:33 -0300

    fix cliente

--- trabalho-poo-01/codigo/Cliente.cs
+++ trabalho-poo-01/codigo/Cliente.cs
@@ -1,40 +1,14 @@
 class Cliente
 {
 
-    public string Nome { get; set; }
-
-    private Restaurante restaurante;
+    public string nome { get; set; }
 
     /// <summary>
-    /// Inicializa uma nova instância da classe Cliente com o restaurante fornecido.
+    /// Inicializa uma nova instância da classe Cliente com o nome fornecido.
     /// </summary>
-    /// <param name="restaurante">O restaurante no qual o cliente deseja fazer a reserva.</param>
-
-    public Cliente(Restaurante restaurante)
+    public Cliente(string nome)
     {
-        this.restaurante = restaurante;
-    }
-
-    /// <summary>
-    /// Tenta fazer uma reserva de mesa para o cliente no restaurante.
-    /// </summary>
-    /// <param name="qtdPessoas">O número de pessoas para as quais a mesa será reservada.</param>
-    /// <param name="nome">O nome do cliente.</param>
-    /// <returns>Um valor booleano que indica se a mesa foi reservada com sucesso.</returns>
-    public bool PedirMesa(int qtdPessoas, string nome)
-    {
-
-        bool mesaAlocada = restaurante.AlocarMesa(qtdPessoas, nome);
-        if (mesaAlocada)
-        {
-            Console.WriteLine($"Mesa alocada para {nome} para {qtdPessoas} pessoas.");
-            return true;
-        }
-        else
-        {
-            Console.WriteLine($"Não foi possível alocar uma mesa para {nome}.");
-            return false;
-        }
+        this.nome = nome;
     }
 
 }
